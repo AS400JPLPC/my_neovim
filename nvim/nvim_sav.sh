31fb2795c3dbd1d30fc2d267002b95e2506b67a7
AS400JPLPC <laroche.jeanpierre@gmail.com>
2026-09-05 16:36:49 +0200

    maj_20260905_16:35

--- nvim/nvim_sav.sh
+++ nvim/nvim_sav.sh
@@ -31,7 +31,6 @@ cp ~/.config/nvim/init.lua ~/GIT/my_neovim/nvim/
 cp ~/nvim_sav.sh ~/GIT/my_neovim/nvim/
 cp ~/nvim_insatll.sh ~/GIT/my_neovim/nvim/
 cp ~/get_plugins.sh ~/GIT/my_neovim/nvim/
-cp ~/update_plugins.sh ~/GIT/my_neovim/nvim/
 cp ~/.config/nvim/Neovim.png ~/GIT/my_neovim/nvim/
 cp ~/.config/nvim/Tree.png ~/GIT/my_neovim/nvim/
 cp ~/.config/nvim/fonction.odt ~/GIT/my_neovim/nvim/
